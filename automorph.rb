826e96b16ee38e5b1486efd6d5d833dff8cbfe59
povilasmk <povilasmk@gmail.com>
2016-05-18 20:53:26 +0300

    papildyta

--- automorph.rb
+++ automorph.rb
@@ -1,3 +1,34 @@
 class Automorph
+=begin
+In mathematics an automorphic number
+(sometimes referred to as a circular number) is a number whose
+square "ends" in the same digits as the number itself.
+For example, 52 = 25, 62 = 36, 762 = 5776
+=end
+
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+
+  def valid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
